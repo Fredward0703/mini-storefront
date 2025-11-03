3666818704711ee381ace8b8bbbefe6edf74ca29
Federico Montejo <fmontejo@usf.edu>
2025-11-03 00:09:07 -0500

    Made a function to filter out the different categories in the CategoryFilter file

--- src/app/components/CategoryFilter.jsx
+++ src/app/components/CategoryFilter.jsx
@@ -1,1 +1,12 @@
 'use client';
+
+export default function CategoryFilter({ category, setCategory}) {
+    return (
+        <select value={category} onChange={e => setCategory(e.target.value)} style={{margainRight:"10px"}}>
+        <option value="All">All</option>
+        <option value="Electornics">Electornics</option>
+        <option value="Clothing">Clothing</option>
+        <option value="Furniture">Furniture</option>
+        </select>
+    );
+}
