7df776f7d3ba67168d6589d14644d371651f78ed
Federico Montejo <fmontejo@usf.edu>
2025-11-03 00:01:38 -0500

    Added a function in ProductCard to display the items along with its details

--- src/app/components/ProductCard.jsx
+++ src/app/components/ProductCard.jsx
@@ -1,1 +1,18 @@
 'use client';
+
+export default function ProductCard({ product, onAddToCart }) {
+    return (
+        <>
+        <h2>{product.name}</h2>
+        <p>Category: {product.category}</p>
+        <p>Price: ${product.price}</p>
+        <p>Stock: {product.stock}</p>
+        <button 
+            onClick={() => onAddToCart(product)}
+            disabled={product.stock === 0} 
+        >
+            {product.stock === 0 ? "Out of stock" : "Add to cart"}
+        </button>
+        </>
+    );
+}
