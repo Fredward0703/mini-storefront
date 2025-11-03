0631ef61d413311f81be71e3a7ac8bfe56305426
Federico Montejo <fmontejo@usf.edu>
2025-11-02 23:51:33 -0500

    Added the product list function into the productList file to display products

--- src/app/components/ProductList.jsx
+++ src/app/components/ProductList.jsx
@@ -1,1 +1,13 @@
 'use client';
+
+import ProductCard from "./ProductCard";
+
+export default function ProductList({ prodcuts, onAddToCart }) {
+    return (
+        <>
+        {prodcuts.map(product => (
+            <ProductCard key={product.id} product={product} onAddToCart={onAddToCart} />
+        ))}
+        </>
+    );
+}
