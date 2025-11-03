1ea8b02f3ac4cf2d3f74555a5be5e38d675d92f6
Federico Montejo <fmontejo@usf.edu>
2025-11-03 00:13:16 -0500

    Added a function to filter the price

--- src/app/components/PriceFilter.jsx
+++ src/app/components/PriceFilter.jsx
@@ -1,1 +1,12 @@
 'use client';
+
+export default function PriceFilter({ maxPrice, setMaxPrice }) {
+    return (
+        <input
+        type="number"
+        placeholder="Max Price"
+        value={maxPrice}
+        onChange={e => setMaxPrice(e.target.value)}
+        />
+    );
+}
