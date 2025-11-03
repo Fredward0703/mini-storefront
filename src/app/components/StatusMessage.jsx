cee3a230630079dd5a069abbaa5354591e4c536f
Federico Montejo <fmontejo@usf.edu>
2025-11-02 23:37:48 -0500

    Added a function ot the statusmessage file for loading and errors

--- src/app/components/StatusMessage.jsx
+++ src/app/components/StatusMessage.jsx
@@ -1,1 +1,9 @@
 'use client';
+
+export default function StatusMessage({ message }) {
+    return (
+        <div style={{ textAlign: "center", padding:"20px" }}>
+        <p>{message}</p>
+        </ div>
+    );
+}
